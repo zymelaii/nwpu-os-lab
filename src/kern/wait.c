dc8ae5087466cc873ed5b3f131a1e7cfa79a2df1
周上行Ryer <melaiiryer@gmail.com>
2023-12-14 01:06:41 +0800

    feat(lab6): impl wait

--- src/kern/wait.c
+++ src/kern/wait.c
@@ -1,31 +1,122 @@
 #include <assert.h>
+#include <string.h>
 #include <mmu.h>
 #include <type.h>
+#include <x86.h>
+#include <errno.h>
+#include <kern/trap.h>
+#include <kern/sche.h>
+#include <kern/process.h>
+#include <kern/kmalloc.h>
+#include <kern/pmap.h>
+
+static son_node_t* find_zombie_proc(son_node_t *node) {
+	static u32 lock = 0;
+	while (xchg(&lock, 1) == 1) { schedule(); }
+	assert(node != NULL);
+	do {
+		if (node->p_son->statu == ZOMBIE) { break; }
+		node = node->nxt;
+	} while (node != NULL);
+	xchg(&lock, 0);
+	return node;
+}
+
+static void remove_proc_link(pcb_t *fa, son_node_t *son) {
+	assert(son != NULL);
+	assert(son->p_son != NULL);
+	son_node_t *pre = son->pre;
+	son_node_t *nxt = son->nxt;
+	//! FIXME: lock is required
+	son->pre = NULL;
+	son->nxt = NULL;
+	if (pre != NULL) {
+		pre->nxt = nxt;
+	}
+	if (nxt != NULL) {
+		nxt->pre = pre;
+	}
+	if (fa != NULL && fa->fork_tree.sons == son) {
+		assert(pre == NULL);
+		fa->fork_tree.sons = nxt;
+	}
+}
 
 ssize_t
 kern_wait(int *wstatus)
 {
-	// 相比于fork来说，wait的实现简单很多
-	// 语义实现比较清晰，没有fork那么多难点要处理，所以这里并不会给大家太多引导
-	// 需要大家自己思考wait怎么实现。
+	ssize_t retval = 0;
 
-	// 在实现之前你必须得读一遍文档`man 2 wait`
-	// 了解到wait大概要做什么
-	panic("Unimplement! Read The F**king Manual");
+	pcb_t *fa = &p_proc_ready->pcb;
+	son_node_t *node = NULL;
+	while (xchg(&fa->lock, 1) == 1) { schedule(); }
 
-	// 当然读文档是一方面，最重要的还是代码实现
-	// wait系统调用与exit系统调用关系密切，所以在实现wait之前需要先读一遍exit为好
-	// 可能读完exit的代码你可能知道wait该具体做什么了
-	panic("Unimplement! Read The F**king Source Code");
+	do {
+		//! NOTE: proc family tree can changed externally, so
+		//> it's necessary to check it every time
+		if (fa->fork_tree.sons == NULL) {
+			retval = -ECHILD;
+			break;
+		}
 
-	// 接下来就是你自己的实现了，我们在设计的时候这段代码不会有太大问题
-	// 在实现完后你任然要对自己来个灵魂拷问
-	// 1. 上锁上了吗？所有临界情况都考虑到了吗？（永远要相信有各种奇奇怪怪的并发问题）
-	// 2. 所有错误情况都判断到了吗？错误情况怎么处理？（RTFM->`man 2 wait`）
-	// 3. 是否所有的资源都正确回收了？
-	// 4. 你写的代码真的符合wait语义吗？
-	panic("Unimplement! soul torture");
-	return 0;
+		node = find_zombie_proc(fa->fork_tree.sons);
+		if (node != NULL) {
+			//! check validity
+			while (xchg(&node->p_son->lock, 1) == 1) { schedule(); }
+			if (node->p_son->fork_tree.p_fa != fa) {
+				node = NULL;
+				xchg(&node->p_son->lock, 0);
+			} else {
+				break;
+			}
+		}
+		//! sleep until awake
+		fa->statu = SLEEPING;
+		//! NOTE: the current proc can be waken up by an exited
+		//> zombie child, but there're still some other cases,
+		//> e.g. transfer_orphans in kern_exit will always wake
+		//> up the 0 proc, so a loop is necessary here
+		//! NOTE: give up the lock since awake from child may
+		//> acquire the lock to modify proc statu
+		xchg(&fa->lock, 0);
+		schedule();
+		while (xchg(&fa->lock, 1) == 1) { schedule(); }
+	} while (1);
+
+	if (node != NULL) {
+		pcb_t *ch = node->p_son;
+
+		assert(ch->fork_tree.p_fa == fa);
+		assert(ch->fork_tree.sons == NULL);
+
+		retval = ch->pid;
+
+		if (wstatus != NULL) {
+			*wstatus = ch->exit_code;
+		}
+
+		remove_proc_link(fa, node);
+		ch->fork_tree.p_fa = NULL;
+		assert(ch->fork_tree.sons == NULL);
+
+		recycle_pages(ch->page_list);
+
+		ch->page_list = NULL;
+		memset(&ch->user_regs, 0, sizeof(user_context_t));
+		memset(&ch->kern_regs, 0, sizeof(kern_context_t));
+		memset(&ch->fork_tree, 0, sizeof(tree_node_t));
+
+		kfree(node);
+		node = NULL;
+
+		ch->statu = IDLE;
+
+		xchg(&ch->lock, 0);
+	}
+
+	xchg(&fa->lock, 0);
+
+	return retval;
 }
 
 ssize_t
